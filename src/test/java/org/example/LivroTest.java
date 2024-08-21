cae7f88fc821aeb645463005ddd66408292475ab
Matheus - Unicesumar <ra-23371012-2@alunos.unicesumar.edu.br>
2024-08-20 22:15:57 -0300

    Implementando testes 1

--- src/test/java/org/example/LivroTest.java
+++ src/test/java/org/example/LivroTest.java
@@ -2,7 +2,8 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 public class LivroTest {
 
@@ -10,5 +11,27 @@ public class LivroTest {
     @Test
     public void testGetTitulo() {
         assertEquals("Casmurro", livro.getTitulo());
+    }
+
+    @Test
+    public void testGetAnoPublicacao(){
+        assertEquals("1899", livro.getAnoPublicacao());
+    }
+
+    @Test
+    public void testGetAutor(){
+        assertEquals("Machado", livro.getAutor());
+    }
+
+    @Test
+    public void testGetDisponibilidade(){
+        assertTrue(livro.isDisponibilidade());
+    }
+
+    @Test
+    public void testEmprestar(){
+        assertTrue(livro.isDisponibilidade());
+        livro.emprestar();
+        assertFalse(livro.isDisponibilidade());
     }
 }
